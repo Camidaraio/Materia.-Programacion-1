e6a9398b4ad96353c05cea157c4f8b818e54e82b
Camila <camiladaraio15@gmail.com>
2024-04-29 08:20:58 -0300

    Resursividad

--- 4.-clase.Funciones_recursivas/Practica/recursivas.py
+++ 4.-clase.Funciones_recursivas/Practica/recursivas.py
@@ -8,27 +8,40 @@ def sumar_naturales(numero:int):
 
 def calcular_potencia(base:int, exponente:int):
     if exponente == 0:
-        exponente = 1
+        resultado = 1
     else:
-        resultado = base ** calcular_potencia(base, exponente - 1)
-        return resultado
-
-
-
-
-
-# print(sumar_naturales(10))
+        resultado = base * calcular_potencia(base, exponente - 1)
+    return resultado
 
 
 def sumar_digitos(numero: int) -> int:
     if numero < 10:
-        return 0
+        resultado = numero
     else:
-        resultado = numero % 10 + sumar_digitos(numero // 10)
+        ultimo_digito = numero % 10
+        digitos_restantes = numero // 10
+        resultado = ultimo_digito + sumar_digitos(digitos_restantes)
 
     return resultado
 
-print(sumar_digitos(3))
+# # Otra manera
+# def sumar_digitos(numero: int) -> int:
+#     if numero < 10:
+#         return 0
+#     else:
+#         resultado = numero % 10 + sumar_digitos(numero // 10)
+
+#     return resultado
+
+
+def calcular_fibonacci(numero):
+    if(numero < 2):
+        resultado = numero
+    else:
+        resultado = calcular_fibonacci(numero - 1) + calcular_fibonacci(numero - 2)
+    return resultado
+
+
 
 
  
