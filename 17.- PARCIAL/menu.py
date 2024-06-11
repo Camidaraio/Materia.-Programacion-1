da3fcb6d73c7e9b5abae4e2a92d1477be2a1bcdb
Camila <camiladaraio15@gmail.com>
2024-06-11 14:24:38 -0300

    11/06

--- 17.- PARCIAL/menu.py
+++ 17.- PARCIAL/menu.py
@@ -1,19 +1,37 @@
 from funciones import *
 
+import csv
 
-lista_empleados = []
+def leer_csv_como_diccionarios(nombre_archivo):
+    """Lee un archivo CSV y lo convierte en una lista de diccionarios."""
+    lista_diccionarios = []
+    with open(nombre_archivo, 'r', newline='', encoding='utf-8') as archivo:
+        lector_csv = csv.DictReader(archivo)
+        for fila in lector_csv:
+            lista_diccionarios.append(fila)
+    return lista_diccionarios
 
-archivo = open("17.- PARCIAL/Proyectos.csv", "r")
-lector_csv = archivo.read()
-for fila in lector_csv:
-    lista_empleados.append(fila)
+# Llamada a la función
+nombre_archivo = '17.- PARCIAL/Proyectos.csv'
+lista_proyectos = leer_csv_como_diccionarios(nombre_archivo)
 
-for empleado in lista_empleados:
-    print(empleado)
-archivo.close()
+# Imprimir la lista de diccionarios
+for proyecto in lista_proyectos:
+    print(proyecto)
+
+#lista_empleados = []
+
+# archivo = open("17.- PARCIAL/Proyectos.csv", "r")
+# lector_csv = archivo.read()
+# for fila in lector_csv:
+#     lista_empleados.append(fila)
+
+# for empleado in lista_empleados:
+#     print(empleado)
+# archivo.close()
 
 while True:
-    print("""1.- Ingresar proyecto \n2.- Modificar proyecto \n3.- Cancelar proyecto \n4.- Comprobar proyectos: \n5.- Mostrar todos \n6.- Calcular presupuesto promedio \n7.- Buscar proyecto por nombre \n8.- Ordenar proyectos \n9.- Retomar proyecto: \n10.- Salir""")
+    print("""1.- Ingresar proyecto \n2.- Modificar proyecto: \n3.- Cancelar proyecto \n4.- Comprobar proyectoss \n5.- Mostrar todos \n6.- Calcular presupuesto promedio \n7.- Buscar proyecto por nombre \n8.- Ordenar proyectos \n9.- Retomar proyecto: \n10.- Salir""")
     
 
     opcion = pedir_entero("Ingrese un valor: ", "ERROR, ingrese una opcion correcta", 1, 10)
